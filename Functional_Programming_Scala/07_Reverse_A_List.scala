75414cb192565125c04bbab2dd12b06f6f5e805f
SarahWagner <sarah.wagner@inwt-statistics.de>
2018-01-12 17:20:20 +0100

    Update 07_Reverse_A_List.scala

--- Functional_Programming_Scala/07_Reverse_A_List.scala
+++ Functional_Programming_Scala/07_Reverse_A_List.scala
@@ -1,4 +1,4 @@
-/* TASK: You are given a list of  elements. Reverse the list without using the reverse function. The input and output portions will be handled automatically. You need to write a function with the recommended method signature. */
+/* TASK: You are given a list of  elements. Reverse the list without using the reverse function. */
 
 def f(arr: List[Int]): List[Int] = {
         
