22c0dfec8136d431f815cf45d1ac6d208cd271f3
Sarah Wagner <sarah.wagner@inwt-statistics.de>
2018-01-18 19:21:13 +0100

    Submission: List Length

---
+++ Functional_Programming_Scala/09_List_Length.scala
@@ -0,0 +1,15 @@
+/* TASK: Count the number of elements in an array without using count, size or length operators (or their equivalents). The input and output portions will be handled automatically by the grader. You only need to write a function with the recommended method signature. */
+
+def f(arr: List[Int]): Int = {
+
+def iter(l: List[Int], n: Int): Int = {
+
+arr match {
+case Nil => n
+case x :: y => iter(l.tail, n + 1) 
+case x :: Nil => n + 1
+}
+
+iter(arr, 0)
+}
+}
